badc26d4e3f3cca72d359675bdf525b2849a1d9d
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-09 17:11:52 +0100

    reviews and all - rake passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,6 @@
  class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
-  # GET /restaurants
-  # GET /restaurants.json
   def index
     @restaurants = Restaurant.all
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,31 @@ restaurants_attributes = [
     name:           "Epicure au Bristol",
     address:        "112 rue du Fg St-Honoré 75008 Paris",
     phone_number:   "0123456789",
-    category:       "japanese"
+    category:       "french"
   },
   {
     name:           "La truffière",
     address:        "4 rue Blainville 75005 Paris",
     phone_number:   "0123456789",
-    category:       "japanese"
+    category:       "french"
   },
   {
     name:           "Le pré catelan",
     address:        "route de Suresnes 75016 Paris",
     phone_number:   "0123456789",
-    category:       "japanese"
+    category:       "french"
+  },
+  {
+    name:           "Wingstop",
+    address:        "180 Kitchener Road #B1-33/34/K15 City Square Mall, Singapore, 208539",
+    phone_number:   "0123456789",
+    category:       "belgian"
+  },
+  {
+    name:           "Mohammad i Restaurant",
+    address:        "5, Lembu Road, Singapore, 208444",
+    phone_number:   "0123456789",
+    category:       "chinese"
   }
 ]
 restaurants_attributes.each { |params| Restaurant.create!(params) }
@@ -34,24 +46,5 @@ reviews_attributes = [
     rating: 5,
     restaurant_id: 1
   },
-  {
-    content: "good value for money",
-    rating: 5,
-    restaurant_id: 1
-  },
-  {
-    content: "good value for money",
-    rating: 5,
-    restaurant_id: 1
-  },
-  {
-    content: "good value for money",
-    rating: 5,
-    restaurant_id: 1
-  },
-  {
-    content: "good value for money",
-    rating: 5,
-    restaurant_id: 1
-  },
 ]
+reviews_attributes.each { |params| Review.create(params) }
